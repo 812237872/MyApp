16e5ca9df68e86e8b51ea29ed7608117b946b083
SuMing6 <1339503821@qq.com>
2019-05-12 21:32:09 +0800

    杨明豪

--- app/src/main/java/com/bw/movie/view/ShowActivity.java
+++ app/src/main/java/com/bw/movie/view/ShowActivity.java
@@ -1,18 +1,69 @@
 package com.bw.movie.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.bw.movie.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ShowActivity extends AppCompatActivity {
+
+    private FilmFragment filmFragment;
+    private CinemaFragment cinemaFragment;
+    private MyFragment myFragment;
+    private RadioGroup radioGroup;
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        radioGroup = findViewById(R.id.Ashow_RadioGroup);
 
+        filmFragment = new FilmFragment();
+        cinemaFragment = new CinemaFragment();
+        myFragment = new MyFragment();
 
+        manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .add(R.id.Ashow_ViewPager,filmFragment)
+                .add(R.id.Ashow_ViewPager,cinemaFragment)
+                .add(R.id.Ashow_ViewPager,myFragment)
+                .show(filmFragment)
+                .hide(cinemaFragment)
+                .hide(myFragment)
+                .commit();
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(android.widget.RadioGroup group, int checkedId) {
+                FragmentTransaction transaction = manager.beginTransaction();
+                switch (checkedId){
+                    case R.id.Ashow_Radio1 :
+                        transaction.show(filmFragment)
+                                .hide(cinemaFragment)
+                                .hide(myFragment);
+                        break;
+                    case R.id.Ashow_Radio2 :
+                        transaction.show(cinemaFragment)
+                                .hide(filmFragment)
+                                .hide(myFragment);
+                        break;
+                    case R.id.Ashow_Radio3 :
+                        transaction.show(myFragment)
+                                .hide(cinemaFragment)
+                                .hide(filmFragment);
+                        break;
+                }
+                transaction.commit();
+            }
+        });
     }
 }
