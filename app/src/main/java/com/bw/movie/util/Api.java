097cc255086e724e9056b121db9a121c27ba4a72
SuMing6 <1339503821@qq.com>
2019-05-13 20:57:07 +0800

    杨明豪

--- app/src/main/java/com/bw/movie/util/Api.java
+++ app/src/main/java/com/bw/movie/util/Api.java
@@ -3,19 +3,25 @@ package com.bw.movie.util;
 import okhttp3.ResponseBody;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
+import rx.Observer;
 
 public interface Api {
     @FormUrlEncoded
     @POST("login")
     public Observable<ResponseBody> getLogin(@Field("phone") String phone , @Field("pwd") String pwd, @Field("pwd2") String pwd2);
+
     @FormUrlEncoded
     @POST("registerUser")
     public Observable<ResponseBody>
     getRegister(@Field("nickName") String nickName , @Field("sex") int sex,
                 @Field("birthday") String birthday , @Field("phone") String phone,
                 @Field("email") String email , @Field("pwd") String pwd, @Field("pwd2") String pwd2);
+
+    @GET("findRecommendCinemas")
+    public Observer<ResponseBody>getRecommend();
 
 }
